ffcce0c9ce68d7c3c9ce39bd53e576b620192ac7
Qədir Mikayılzadə <130463028+Qedir-Mikayilzade@users.noreply.github.com>
2023-12-29 11:02:34 +0400

    Add files via upload

---
+++ React-ders4/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import UserLogin from "./Components/UserLogin";
+
+function App() {
+  return (
+    <div className="App">
+      <UserLogin />
+    </div>
+  );
+}
+
+export default App;
